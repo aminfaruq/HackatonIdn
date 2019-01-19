c8e118aa3ad6599bc57f1b827377ec232f8e72e1
maminfaruq <aminfaruk.fa@gmail.com>
2019-01-19 14:49:32 +0700

    adapter

--- app/src/main/java/id/co/maminfaruq/hackatonidn/adapter/AdapterBerita.java
+++ app/src/main/java/id/co/maminfaruq/hackatonidn/adapter/AdapterBerita.java
@@ -1,5 +1,43 @@
 package id.co.maminfaruq.hackatonidn.adapter;
 
-public class AdapterBerita{
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
+import java.util.List;
+
+import id.co.maminfaruq.hackatonidn.responseapi.ArticlesItem;
+
+public class AdapterBerita extends RecyclerView.Adapter<AdapterBerita.ViewHolder>{
+    private Context context;
+    private List<ArticlesItem>berita;
+
+    public AdapterBerita(Context context, List<ArticlesItem> berita) {
+        this.context = context;
+        this.berita = berita;
+    }
+
+    @NonNull
+    @Override
+    public AdapterBerita.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterBerita.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
