d443356a040b1229eb8a621cd8d8a016f766bd1a
maminfaruq <aminfaruk.fa@gmail.com>
2019-01-19 17:17:15 +0700

    push

--- app/src/main/java/id/co/maminfaruq/hackatonidn/activities/HomeFragment.java
+++ app/src/main/java/id/co/maminfaruq/hackatonidn/activities/HomeFragment.java
@@ -1,21 +1,43 @@
 package id.co.maminfaruq.hackatonidn.activities;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import id.co.maminfaruq.hackatonidn.R;
+import id.co.maminfaruq.hackatonidn.adapter.AdapterBerita;
+import id.co.maminfaruq.hackatonidn.network.ApiService;
+import id.co.maminfaruq.hackatonidn.network.ConfigRetrofit;
+import id.co.maminfaruq.hackatonidn.responseapi.ArticlesItem;
+import id.co.maminfaruq.hackatonidn.responseapi.ResponseBerita;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    Unbinder unbinder;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -26,11 +48,63 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        tampilBerita();
+    }
+
+    private void tampilBerita() {
+        final ProgressDialog dialog = ProgressDialog.show(getContext(), "", "loading. . .");
+        ConfigRetrofit config = new ConfigRetrofit();
+        ApiService api = config.service;
+
+        api.getAllBerita().enqueue(new Callback<ResponseBerita>() {
+            @Override
+            public void onResponse(Call<ResponseBerita> call, Response<ResponseBerita> response) {
+                Toast.makeText(getContext(), response.message(), Toast.LENGTH_SHORT).show();
+                Log.i("debug sukses", response.message());
+
+                if (response.isSuccessful()) {
+                    dialog.dismiss();
+
+                    String status = response.body().getStatus();
+
+                    if (status.equals(status)) {
+                        ResponseBerita dataSemua = response.body();
+
+                        List<ArticlesItem> data_berita = dataSemua.getArticles();
+
+                        AdapterBerita adapterBerita = new AdapterBerita(getContext(),data_berita);
+
+                        recyclerView.setHasFixedSize(true);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                        recyclerView.setAdapter(adapterBerita);
+
+                    }
+
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBerita> call, Throwable t) {
+                Toast.makeText(getContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.e("debug gagal",t.getMessage());
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
--- app/src/main/java/id/co/maminfaruq/hackatonidn/adapter/AdapterBerita.java
+++ app/src/main/java/id/co/maminfaruq/hackatonidn/adapter/AdapterBerita.java
@@ -3,16 +3,26 @@ package id.co.maminfaruq.hackatonidn.adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import id.co.maminfaruq.hackatonidn.R;
 import id.co.maminfaruq.hackatonidn.responseapi.ArticlesItem;
 
-public class AdapterBerita extends RecyclerView.Adapter<AdapterBerita.ViewHolder>{
+public class AdapterBerita extends RecyclerView.Adapter<AdapterBerita.ViewHolder> {
+
+
     private Context context;
-    private List<ArticlesItem>berita;
+    private List<ArticlesItem> berita;
 
     public AdapterBerita(Context context, List<ArticlesItem> berita) {
         this.context = context;
@@ -21,23 +31,37 @@ public class AdapterBerita extends RecyclerView.Adapter<AdapterBerita.ViewHolder
 
     @NonNull
     @Override
-    public AdapterBerita.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.berita_item, viewGroup, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AdapterBerita.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.txtJudulBerita.setText(berita.get(i).getTitle());
+        viewHolder.txtPenulis.setText(berita.get(i).getAuthor());
+        viewHolder.txtTglTerbit.setText(berita.get(i).getPublishedAt());
+        Glide.with(context).load(berita.get(i).getUrlToImage()).into(viewHolder.imgBerita);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return berita.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(@NonNull View itemView) {
+    class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.imgBerita)
+        ImageView imgBerita;
+        @BindView(R.id.txtJudulBerita)
+        TextView txtJudulBerita;
+        @BindView(R.id.txtPenulis)
+        TextView txtPenulis;
+        @BindView(R.id.txtTglTerbit)
+        TextView txtTglTerbit;
+
+        ViewHolder(@NonNull View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
