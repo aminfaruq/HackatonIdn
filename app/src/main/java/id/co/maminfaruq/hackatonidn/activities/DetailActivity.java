46931b08766ffd936ceaa50918861a18301f933f
lutfillahmafazi <luthfimafazi18@gmail.com>
2019-01-19 16:18:18 +0700

    p

---
+++ app/src/main/java/id/co/maminfaruq/hackatonidn/activities/DetailActivity.java
@@ -0,0 +1,16 @@
+package id.co.maminfaruq.hackatonidn.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.co.maminfaruq.hackatonidn.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
